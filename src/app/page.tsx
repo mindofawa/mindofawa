2282b051e3bca5b81338c600ae44eb723d3825c3
mindofawa <alanturak2005@gmail.com>
2024-09-27 07:57:44 +0200

    initial

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import Typography from '@mui/material/Typography';
+export default function Home() {
+  return(
+    <Typography variant="h5" gutterBottom>
+      Hello World!
+    </Typography>
+  )
+}
